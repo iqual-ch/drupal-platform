00951f14097e9fc0cf6b3cad811dd90a8da16ec6
martinstadelmann <m.stadelmann@iqual.ch>
2025-11-04 16:32:24 +0000

    PF-512: Added initial `ddev` setup

--- assets/replace/@web-root/sites/default/settings.php
+++ assets/replace/@web-root/sites/default/settings.php
@@ -215,3 +215,9 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 if (file_exists($app_root . '/' . $site_path . '/services.local.yml')) {
     $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.local.yml';
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
